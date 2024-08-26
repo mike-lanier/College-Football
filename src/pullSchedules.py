0f3153845ad587432f2b050dacacbdd6ff058f7d
mike <laniermike17@gmail.com>
2024-08-26 12:07:21 -0400

    Writing schedule files to S3 bucket instead of locally

--- src/pullSchedules.py
+++ src/pullSchedules.py
@@ -1,12 +1,23 @@
 import requests
 import json
 from uuid import uuid4
+from awsConnect import writeToBucket
 from getWeek import week_start, week_end, week_id
 
 
-response = requests.get('http://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard?dates=' + week_start + '-' + week_end)
-data = response.json()
-file_id = str(uuid4())
+def writeScheduleToS3Bucket():
+    response = requests.get('http://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard?dates=' + week_start + '-' + week_end)
+    json_data = response.json()
+    file_data = json.dumps(json_data)
 
-with open('./data/schedule_files/' + week_id + '_' + file_id + '.json', 'w') as f:
-    json.dump(data, f, indent=4)
+    file_id = str(uuid4())
+    file_name = week_id + '_' + file_id
+
+    bucket = 'cfb_s3_bucket'
+    folder = 'schedules/'
+
+    writeToBucket(file_data, file_name, bucket, folder)
+
+
+if __name__ == '__main__':
+    writeScheduleToS3Bucket()
