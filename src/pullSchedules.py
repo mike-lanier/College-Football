3ce689d74946c8c9b2691d392f3142db7cf713cf
mike <laniermike17@gmail.com>
2024-08-27 21:46:28 -0400

    Added exception handling

--- src/pullSchedules.py
+++ src/pullSchedules.py
@@ -4,20 +4,27 @@ from awsConnect import connectS3, getBucketName
 from getWeek import week_start, week_end, week_id
 
 
-def writeScheduleToS3Bucket():
-    response = requests.get('http://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard?dates=' + week_start + '-' + week_end)
-    json_data = response.json()
-    file_data = json.dumps(json_data)
+def create_schedule_file():
+    try:
+        response = requests.get('http://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard?dates=' + week_start + '-' + week_end)
+        if response.status_code == 200:
+            json_data = response.json()
+            file_data = json.dumps(json_data)
+        else:
+            print("Request Failed")
+            return
 
-    bucket = getBucketName('cfb_s3_bucket')
+        bucket = getBucketName('cfb_s3_bucket')
+        folder = 'schedules/'
+        file_name = folder + week_id + '.json'
+        
+        s3 = connectS3()
+        s3.put_object(Bucket=bucket, Key=file_name, Body=file_data, ContentType='application/json')
+        print(f"[{file_name}] successfully created and uploaded")
 
-    folder = 'schedules/'
-    file_name = folder + week_id + '.json'
-    
-    s3 = connectS3()
-    s3.put_object(Bucket=bucket, Key=file_name, Body=file_data, ContentType='application/json')
-
+    except Exception as e:
+        print(f"Operation failed: {e}")
 
 
 if __name__ == '__main__':
-    writeScheduleToS3Bucket()
+    create_schedule_file()
